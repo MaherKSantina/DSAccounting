bf217f944f064fa903a52b517b90ed464719ae45
Maher Santina <maher.santina90@gmail.com>
2019-11-25 20:40:33 +1100

    Removed unused code

--- Sources/DSAccounting/configure.swift
+++ Sources/DSAccounting/configure.swift
@@ -9,56 +9,12 @@ import Foundation
 import Vapor
 import FluentMySQL
 
-public protocol DSAccountingDatabaseConfigurable {
-    var dsAccountingDatabaseHostName: String { get }
-    var dsAccountingDatabasePort: Int { get }
-    var dsAccountingDatabaseUsername: String { get }
-    var dsAccountingDatabasePassword: String { get }
-    var dsAccountingDatabaseDatabase: String { get }
-    var dsAccountingDatabaseTestDatabase: String { get }
-}
-
 public class DSAccountingMain {
 
-    let dataSource: DSAccountingDatabaseConfigurable
+    public init() { }
 
-    public init(dataSource: DSAccountingDatabaseConfigurable) {
-        self.dataSource = dataSource
-    }
+    public func accountingConfigure(migrations: inout MigrationConfig) throws {
 
-    public func accountingConfigure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-        // Register providers first
-        try services.register(FluentProvider())
-        try services.register(MySQLProvider())
-
-        var commandConfig = CommandConfig.default()
-        commandConfig.useFluentCommands()
-        services.register(commandConfig)
-
-        var databases = DatabasesConfig()
-
-        var databaseName: String = ""
-        switch env {
-        case .testing:
-            databaseName = dataSource.dsAccountingDatabaseTestDatabase
-        default:
-            databaseName = dataSource.dsAccountingDatabaseDatabase
-        }
-
-        let mysql = MySQLDatabase(config: MySQLDatabaseConfig(
-            hostname: dataSource.dsAccountingDatabaseHostName,
-            port: dataSource.dsAccountingDatabasePort,
-            username: dataSource.dsAccountingDatabaseUsername,
-            password: dataSource.dsAccountingDatabasePassword,
-            database: databaseName
-            )
-        )
-        databases.add(database: mysql, as: .mysql)
-        services.register(databases)
-
-
-        // Configure migrations
-        var migrations = MigrationConfig()
         migrations.add(migration: EnableReferencesMigration.self, database: .mysql)
 
         migrations.add(model: InvoiceRow.self, database: .mysql)
@@ -69,7 +25,5 @@ public class DSAccountingMain {
         migrations.add(model: QuotationRow.self, database: .mysql)
         migrations.add(migration: QuotationItemRow.self, database: .mysql)
         migrations.add(migration: Quotation_QuotationItemRow.self, database: .mysql)
-
-        services.register(migrations)
     }
 }
